094fcfa7f8732386c9afa7af2a845f12b9be0245
nobody <thegreatestemailname@gmail.com>
2022-11-01 20:22:15 +0300

    added controller, test and simple html page

--- src/test/java/com/example/springbootinaction/SpringBootInActionApplicationTests.java
+++ src/test/java/com/example/springbootinaction/SpringBootInActionApplicationTests.java
@@ -1,13 +1,40 @@
 package com.example.springbootinaction;
 
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 class SpringBootInActionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    @SneakyThrows
+    public void testHomePage() {
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content()
+                        .string(containsString("Welcome to..."))
+                );
+    }
 
 }
