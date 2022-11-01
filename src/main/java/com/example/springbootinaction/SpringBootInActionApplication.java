8b14739737808cce6b010951b3b464a56eed7399
cringecoder <thegreatestemailname@gamil.com>
2022-11-01 19:24:57 +0300

    first commit

---
+++ src/main/java/com/example/springbootinaction/SpringBootInActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootinaction/SpringBootInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
