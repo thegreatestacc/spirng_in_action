6787d0b7b2b808674609551643ca889e136064d2
nobody <thegreatestemailname@gmail.com>
2022-11-05 08:11:24 +0300

    changes lesson 3

--- src/main/java/com/example/springbootinaction/controller/DesignTacoController.java
+++ src/main/java/com/example/springbootinaction/controller/DesignTacoController.java
@@ -1,15 +1,21 @@
 package com.example.springbootinaction.controller;
 
+import com.example.springbootinaction.model.Design;
 import com.example.springbootinaction.model.Ingredient;
+import com.example.springbootinaction.model.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 import com.example.springbootinaction.model.Ingredient.Type;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -42,4 +48,12 @@ public class DesignTacoController {
 */
         return "design";
     }
+
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors) {
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
+    
+
 }
---
+++ src/main/java/com/example/springbootinaction/model/Design.java
@@ -0,0 +1,7 @@
+package com.example.springbootinaction.model;
+
+import lombok.Data;
+
+@Data
+public class Design {
+}
